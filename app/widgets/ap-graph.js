946073024f2a4bb6bb57550e04f88ce36bd5d456
Jason Cheok Wan <jason@nyansa.com>
2017-02-13 16:07:45 -0800

    refactoring widgets

--- app/widgets/ap-graph.js
+++ app/widgets/ap-graph.js
@@ -1,202 +1,204 @@
-'use strict';
+(function(angular) {
+  'use strict';
 
-angular.module('widgets')
+  angular.module('widgets')
 
-.component('apGraph', {
-  restrict: 'E',
-  replace: true,
-  templateUrl: 'widgets/ap-graph-tpl.html',
-  bindings: {
-    data: '<'
-  },
-  controller: ['$element', '$interpolate', '$timeout', '$scope', ApGraphController]
-});
-
-var nodeTemplate =
-"\
-<table>\
-  <tbody>\
-    <tr>\
-      <td>AP Name</td>\
-      <td>{{ d.attrs.apName }}</td>\
-    </tr>\
-    <tr>\
-      <td>Ap Group</td>\
-      <td>{{ d.attrs.apGroup }}</td>\
-    </tr>\
-    <tr>\
-      <td>Num Clients</td>\
-      <td>{{ d.attrs.numDevices }}</td>\
-    </tr>\
-  </tbody>\
-</table>\
-";
-
-var linkTemplate =
-"\
-<table>\
-  <tbody>\
-    <tr>\
-      <td>Node 1</td>\
-      <td>{{ d.source.attrs.apName }}</td>\
-    </tr>\
-    <tr>\
-      <td>Node 2</td>\
-      <td>{{ d.target.attrs.apName }}</td>\
-    </tr>\
-    <tr>\
-      <td>SNR dB</td>\
-      <td>{{ d.attrs.snrDb }}</td>\
-    </tr>\
-  </tbody>\
-</table>\
-";
-
-function ApGraphController($element, $interpolate, $timeout, $scope) {
-  var $ctrl = this;
-  var svg, width, height, graph, gRect, gContainer, gLinks, gNodes, simulation;
-
-  var color = d3.scale.category20();
-  var radius = d3.scale.sqrt()
-    .domain([0, 100])
-    .range([1, 20]);
-
-  var edgeDistance = d3.scale.linear()
-    .domain([50, 0])
-    .range([1, 20]);
-
-  var edgeColor = d3.scale.linear()
-    .domain([50, 0])
-    .range(['red', 'blue'])
-    .interpolate(d3.interpolateHcl);
-
-  var tooltip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d) {
-      var tooltipHtml;
-      if (d.source && d.target) {
-        tooltipHtml = $interpolate(linkTemplate)({ d: d });
-      } else {
-        tooltipHtml = $interpolate(nodeTemplate)({ d: d });
-      }
-      return tooltipHtml;
-    });
-
-  $scope.$on('tabChanged', function(event, tabIndex) {
-    if (tabIndex == 0) {
-      $timeout(redraw);
-    }
+  .component('apGraph', {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'widgets/ap-graph-tpl.html',
+    bindings: {
+      data: '<'
+    },
+    controller: ['$element', '$interpolate', '$timeout', '$scope', ApGraphController]
   });
 
-  $ctrl.$onInit = function() {
-    svg = d3.select($element.find('svg')[0]);
+  var nodeTemplate =
+  "\
+  <table>\
+    <tbody>\
+      <tr>\
+        <td>AP Name</td>\
+        <td>{{ d.attrs.apName }}</td>\
+      </tr>\
+      <tr>\
+        <td>Ap Group</td>\
+        <td>{{ d.attrs.apGroup }}</td>\
+      </tr>\
+      <tr>\
+        <td>Num Clients</td>\
+        <td>{{ d.attrs.numDevices }}</td>\
+      </tr>\
+    </tbody>\
+  </table>\
+  ";
 
-    gRect = svg.append('rect')
-      .style('fill', 'none')
-      .style('pointer-events', 'all')
-      .call(d3.behavior.zoom()
-        .scaleExtent([1 / 2, 2])
-        .on('zoom', zoomed));
+  var linkTemplate =
+  "\
+  <table>\
+    <tbody>\
+      <tr>\
+        <td>Node 1</td>\
+        <td>{{ d.source.attrs.apName }}</td>\
+      </tr>\
+      <tr>\
+        <td>Node 2</td>\
+        <td>{{ d.target.attrs.apName }}</td>\
+      </tr>\
+      <tr>\
+        <td>SNR dB</td>\
+        <td>{{ d.attrs.snrDb }}</td>\
+      </tr>\
+    </tbody>\
+  </table>\
+  ";
 
-    gContainer = svg.append('g')
-      .call(tooltip);
+  function ApGraphController($element, $interpolate, $timeout, $scope) {
+    var $ctrl = this;
+    var svg, width, height, graph, gRect, gContainer, gLinks, gNodes, simulation;
 
-    gLinks = gContainer.append('g')
-      .attr('class', 'links');
-    gNodes = gContainer.append('g')
-      .attr('class', 'nodes');
+    var color = d3.scale.category20();
+    var radius = d3.scale.sqrt()
+      .domain([0, 100])
+      .range([1, 20]);
 
-    simulation = d3.layout.force()
-      .gravity(0.05)
-      .distance(50)
-      .charge(-100)
-      // .linkDistance(function(d) { return edgeDistance(d.attrs.snrDb); })
-      .on('tick', ticked);
+    var edgeDistance = d3.scale.linear()
+      .domain([50, 0])
+      .range([1, 20]);
 
-    d3.select(window).on('resize', redraw);
-  }
+    var edgeColor = d3.scale.linear()
+      .domain([50, 0])
+      .range(['red', 'blue'])
+      .interpolate(d3.interpolateHcl);
 
-  $ctrl.$onChanges = function() {
-    if ($ctrl.data) {
-      if (!svg) {
-        // let the init code run first
-        return $timeout($ctrl.$onChanges);
-      }
-
-      graph = JSON.parse(JSON.stringify($ctrl.data));
-
-      // prep for D3 force layout
-      _.each(graph.edges, function(e) {
-        e.source = _.findIndex(graph.vertices, function(v) { return v.id == e.source; });
-        e.target = _.findIndex(graph.vertices, function(v) { return v.id == e.target; });
+    var tooltip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        var tooltipHtml;
+        if (d.source && d.target) {
+          tooltipHtml = $interpolate(linkTemplate)({ d: d });
+        } else {
+          tooltipHtml = $interpolate(nodeTemplate)({ d: d });
+        }
+        return tooltipHtml;
       });
 
-      redraw();
-      simulation.start();
+    $scope.$on('tabChanged', function(event, tabIndex) {
+      if (tabIndex == 0) {
+        $timeout(redraw);
+      }
+    });
+
+    $ctrl.$onInit = function() {
+      svg = d3.select($element.find('svg')[0]);
+
+      gRect = svg.append('rect')
+        .style('fill', 'none')
+        .style('pointer-events', 'all')
+        .call(d3.behavior.zoom()
+          .scaleExtent([1 / 2, 2])
+          .on('zoom', zoomed));
+
+      gContainer = svg.append('g')
+        .call(tooltip);
+
+      gLinks = gContainer.append('g')
+        .attr('class', 'links');
+      gNodes = gContainer.append('g')
+        .attr('class', 'nodes');
+
+      simulation = d3.layout.force()
+        .gravity(0.05)
+        .distance(50)
+        .charge(-100)
+        // .linkDistance(function(d) { return edgeDistance(d.attrs.snrDb); })
+        .on('tick', ticked);
+
+      d3.select(window).on('resize', redraw);
+    }
+
+    $ctrl.$onChanges = function() {
+      if ($ctrl.data) {
+        if (!svg) {
+          // let the init code run first
+          return $timeout($ctrl.$onChanges);
+        }
+
+        graph = JSON.parse(JSON.stringify($ctrl.data));
+
+        // prep for D3 force layout
+        _.each(graph.edges, function(e) {
+          e.source = _.findIndex(graph.vertices, function(v) { return v.id == e.source; });
+          e.target = _.findIndex(graph.vertices, function(v) { return v.id == e.target; });
+        });
+
+        redraw();
+        simulation.start();
+      }
+    }
+
+    function redraw() {
+      var clientRect = svg.node().getBoundingClientRect();
+      width = clientRect.width;
+      height = clientRect.height;
+
+      if (!graph) {
+        return;
+      }
+
+      gRect
+        .attr('width', width)
+        .attr('height', height);
+
+      gLinks.call(links, graph);
+      gNodes.call(nodes, graph);
+
+      simulation
+        .size([width, height])
+        .nodes(graph.vertices)
+        .links(graph.edges);
+    }
+
+    function links(g, graph) {
+      var gLines = g.selectAll('line').data(graph.edges);
+      gLines.enter().append('line')
+        .on('mouseover', tooltip.show)
+        .on('mouseout', tooltip.hide);
+      // gLines
+        // .attr('stroke', function(d) { return edgeColor(d.attrs.snrDb); });
+        // .attr('stroke-width', function(d) { return Math.sqrt(d.attrs.snrDb); });
+      gLines.exit().remove();
+    }
+
+    function nodes(g, graph) {
+      var gCircles = g.selectAll('circle').data(graph.vertices, function(d) { return d.id; });
+      gCircles.enter().append('circle')
+        .call(simulation.drag)
+        .on('mouseover', tooltip.show)
+        .on('mouseout', tooltip.hide)
+        .append('title')
+          .text(function(d) { return d.attrs.apName; });
+      gCircles
+        .attr('r', function(d) { return radius(d.attrs.numDevices); })
+        .attr('fill', function(d) { return color(d.attrs.apGroup); });
+      gCircles.exit().remove();
+    }
+
+    function ticked() {
+      gLinks.selectAll('line')
+        .attr('x1', function(d) { return d.source.x; })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
+
+      gNodes.selectAll('circle')
+        .attr('cx', function(d) { return d.x; })  // = Math.max(5, Math.min(width - 5, d.x)); })
+        .attr('cy', function(d) { return d.y; }); // = Math.max(5, Math.min(height - 5, d.y)); });
+    }
+
+    function zoomed() {
+      gContainer.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
     }
   }
-
-  function redraw() {
-    var clientRect = svg.node().getBoundingClientRect();
-    width = clientRect.width;
-    height = clientRect.height;
-
-    if (!graph) {
-      return;
-    }
-
-    gRect
-      .attr('width', width)
-      .attr('height', height);
-
-    gLinks.call(links, graph);
-    gNodes.call(nodes, graph);
-
-    simulation
-      .size([width, height])
-      .nodes(graph.vertices)
-      .links(graph.edges);
-  }
-
-  function links(g, graph) {
-    var gLines = g.selectAll('line').data(graph.edges);
-    gLines.enter().append('line')
-      .on('mouseover', tooltip.show)
-      .on('mouseout', tooltip.hide);
-    // gLines
-      // .attr('stroke', function(d) { return edgeColor(d.attrs.snrDb); });
-      // .attr('stroke-width', function(d) { return Math.sqrt(d.attrs.snrDb); });
-    gLines.exit().remove();
-  }
-
-  function nodes(g, graph) {
-    var gCircles = g.selectAll('circle').data(graph.vertices, function(d) { return d.id; });
-    gCircles.enter().append('circle')
-      .call(simulation.drag)
-      .on('mouseover', tooltip.show)
-      .on('mouseout', tooltip.hide)
-      .append('title')
-        .text(function(d) { return d.attrs.apName; });
-    gCircles
-      .attr('r', function(d) { return radius(d.attrs.numDevices); })
-      .attr('fill', function(d) { return color(d.attrs.apGroup); });
-    gCircles.exit().remove();
-  }
-
-  function ticked() {
-    gLinks.selectAll('line')
-      .attr('x1', function(d) { return d.source.x; })
-      .attr('y1', function(d) { return d.source.y; })
-      .attr('x2', function(d) { return d.target.x; })
-      .attr('y2', function(d) { return d.target.y; });
-
-    gNodes.selectAll('circle')
-      .attr('cx', function(d) { return d.x; })  // = Math.max(5, Math.min(width - 5, d.x)); })
-      .attr('cy', function(d) { return d.y; }); // = Math.max(5, Math.min(height - 5, d.y)); });
-  }
-
-  function zoomed() {
-    gContainer.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
-  }
-}
+})(window.angular);
--- app/widgets/ap-zooming.js
+++ app/widgets/ap-zooming.js
@@ -1,129 +1,131 @@
-'use strict';
+(function(angular) {
+  'use strict';
 
-angular.module('widgets')
+  angular.module('widgets')
 
-.component('apZooming', {
-  restrict: 'E',
-  replace: true,
-  templateUrl: 'widgets/ap-zooming-tpl.html',
-  bindings: {
-    data: '<'
-  },
-  controller: ['$element', '$timeout', '$scope', ApZoomingController]
-});
-
-function ApZoomingController($element, $timeout, $scope) {
-  var $ctrl = this;
-  var svg, gRoot, packLayout, width, height, rootRadius, treeRoot, zoomedNode;
-
-  var x, y;
-
-  $scope.$on('tabChanged', function(event, tabIndex) {
-    if (tabIndex == 1) {
-      $timeout(redraw);
-    }
+  .component('apZooming', {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'widgets/ap-zooming-tpl.html',
+    bindings: {
+      data: '<'
+    },
+    controller: ['$element', '$timeout', '$scope', ApZoomingController]
   });
 
-  $ctrl.$onInit = function() {
-    svg = d3.select($element.find('svg')[0]);
+  function ApZoomingController($element, $timeout, $scope) {
+    var $ctrl = this;
+    var svg, gRoot, packLayout, width, height, rootRadius, treeRoot, zoomedNode;
 
-    gRoot = svg.append('g')
-      .on('click', function() { zoom(treeRoot); });
+    var x, y;
 
-    packLayout = d3.layout.pack()
-      .value(function(d) {
-        return d.attrs.numDevices;
-      });
-
-    // d3.select(window).on('resize', redraw);
-  }
-
-  $ctrl.$onChanges = function() {
-    if ($ctrl.data) {
-      if (!svg) {
-        // let the init code run first
-        return $timeout($ctrl.$onChanges);
+    $scope.$on('tabChanged', function(event, tabIndex) {
+      if (tabIndex == 2) {
+        $timeout(redraw);
       }
+    });
 
-      var graph = JSON.parse(JSON.stringify($ctrl.data));
+    $ctrl.$onInit = function() {
+      svg = d3.select($element.find('svg')[0]);
 
-      var apGroups = _.chain(graph.vertices)
-        .groupBy(function(v) {
-          return v.attrs.apGroup;
-        })
-        .mapObject(function(vs, apGroup) {
-          return { name: apGroup, children: vs };
-        })
-        .values()
-        .value();
+      gRoot = svg.append('g')
+        .on('click', function() { zoom(treeRoot); });
 
-      treeRoot = {
-        name: "Brandeis",
-        children: apGroups
-      };
+      packLayout = d3.layout.pack()
+        .value(function(d) {
+          return d.attrs.numDevices;
+        });
 
-      redraw();
+      // d3.select(window).on('resize', redraw);
+    }
+
+    $ctrl.$onChanges = function() {
+      if ($ctrl.data) {
+        if (!svg) {
+          // let the init code run first
+          return $timeout($ctrl.$onChanges);
+        }
+
+        var graph = JSON.parse(JSON.stringify($ctrl.data));
+
+        var apGroups = _.chain(graph.vertices)
+          .groupBy(function(v) {
+            return v.attrs.apGroup;
+          })
+          .mapObject(function(vs, apGroup) {
+            return { name: apGroup, children: vs };
+          })
+          .values()
+          .value();
+
+        treeRoot = {
+          name: "Brandeis",
+          children: apGroups
+        };
+
+        redraw();
+      }
+    }
+
+    function redraw() {
+      var clientRect = svg.node().getBoundingClientRect();
+      width = clientRect.width;
+      height = clientRect.height;
+      rootRadius = _.min([width, height]) * 0.8;
+
+      x = d3.scale.linear().range([0, rootRadius]);
+      y = d3.scale.linear().range([0, rootRadius]);
+
+      packLayout.size([rootRadius, rootRadius]);
+      var nodes = packLayout.nodes(treeRoot);
+      zoomedNode = treeRoot;
+
+      gRoot
+        .attr('transform', 'translate(' + (width - rootRadius) / 2 + ',' + (height - rootRadius) / 2 + ')');
+
+      var gCircles = gRoot.selectAll('circle')
+          .data(nodes);
+      gCircles.enter().append('circle');
+      gCircles
+        .attr('class', function(d) { return d.children ? 'parent' : 'child'; })
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; })
+        .attr('r', function(d) { return d.r; })
+        .on('click', function(d) { return zoom(d == zoomedNode ? treeRoot : d); });
+
+      var gText = gRoot.selectAll('text')
+          .data(nodes);
+      gText.enter().append('text')
+        .attr('dy', '.35em')
+        .attr('text-anchor', 'middle');
+      gText
+        .attr('class', function(d) { return d.children ? 'parent' : 'child'; })
+        .attr('x', function(d) { return d.x; })
+        .attr('y', function(d) { return d.y; })
+        .style('opacity', function(d) { return d.r > 20 ? 1 : 0; })
+        .text(function(d) { return d.name || d.attrs.apName; });
+    }
+
+    function zoom(d, i) {
+      var k = rootRadius / d.r / 2;
+      x.domain([d.x - d.r, d.x + d.r]);
+      y.domain([d.y - d.r, d.y + d.r]);
+
+      var t = gRoot.transition()
+        .duration(d3.event.altKey ? 7500 : 750);
+
+      t.selectAll("circle")
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .attr("r", function(d) { return k * d.r; });
+
+      t.selectAll("text")
+        .attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
+
+      zoomedNode = d;
+      d3.event.stopPropagation();
     }
   }
-
-  function redraw() {
-    var clientRect = svg.node().getBoundingClientRect();
-    width = clientRect.width;
-    height = clientRect.height;
-    rootRadius = _.min([width, height]) * 0.8;
-
-    x = d3.scale.linear().range([0, rootRadius]);
-    y = d3.scale.linear().range([0, rootRadius]);
-
-    packLayout.size([rootRadius, rootRadius]);
-    var nodes = packLayout.nodes(treeRoot);
-    zoomedNode = treeRoot;
-
-    gRoot
-      .attr('transform', 'translate(' + (width - rootRadius) / 2 + ',' + (height - rootRadius) / 2 + ')');
-
-    var gCircles = gRoot.selectAll('circle')
-        .data(nodes);
-    gCircles.enter().append('circle');
-    gCircles
-      .attr('class', function(d) { return d.children ? 'parent' : 'child'; })
-      .attr('cx', function(d) { return d.x; })
-      .attr('cy', function(d) { return d.y; })
-      .attr('r', function(d) { return d.r; })
-      .on('click', function(d) { return zoom(d == zoomedNode ? treeRoot : d); });
-
-    var gText = gRoot.selectAll('text')
-        .data(nodes);
-    gText.enter().append('text')
-      .attr('dy', '.35em')
-      .attr('text-anchor', 'middle');
-    gText
-      .attr('class', function(d) { return d.children ? 'parent' : 'child'; })
-      .attr('x', function(d) { return d.x; })
-      .attr('y', function(d) { return d.y; })
-      .style('opacity', function(d) { return d.r > 20 ? 1 : 0; })
-      .text(function(d) { return d.name || d.attrs.apName; });
-  }
-
-  function zoom(d, i) {
-    var k = rootRadius / d.r / 2;
-    x.domain([d.x - d.r, d.x + d.r]);
-    y.domain([d.y - d.r, d.y + d.r]);
-
-    var t = gRoot.transition()
-      .duration(d3.event.altKey ? 7500 : 750);
-
-    t.selectAll("circle")
-      .attr("cx", function(d) { return x(d.x); })
-      .attr("cy", function(d) { return y(d.y); })
-      .attr("r", function(d) { return k * d.r; });
-
-    t.selectAll("text")
-      .attr("x", function(d) { return x(d.x); })
-      .attr("y", function(d) { return y(d.y); })
-      .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
-
-    zoomedNode = d;
-    d3.event.stopPropagation();
-  }
-}
+})(window.angular);
